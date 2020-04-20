c954ef53edf5bc12d7b9ff1de127d0540b7330b3
Andrea <Andrea@MAGGIPCPORT08.maggi.it>
2020-04-20 16:42:07 +0200

    Updated release to 1.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Popolo Italiano'
 author = 'Popolo Italiano'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
